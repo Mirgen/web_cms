3e819ea5339c35b669fa86dd314c1cd4550262eb
kvapij02 <jiri.kvapil@dixonsretail.com>
2015-10-19 00:56:10 +0200

    Initial module commit. Added all the necessary files for new module.

---
+++ app/ModulesModule/Menu/FrontModule/presenters/ModuleMenuPresenter.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+namespace App\FrontModule\Presenters;
+
+/**
+ * Description of TextEditor
+ *
+ * @author Jiri Kvapil
+ */
+
+class ModuleMenuPresenter extends ModuleBasePresenter
+{
+    private $articles = "";
+
+    protected $moduleTemplateDir = "Menu";
+
+    protected function loadModuleData(){
+        if($this->oParentPresenter){
+            $this->articles = array(
+                array("title" => "Article one", "text" => "Lorem ipsum dolor sit amet."),
+                array("title" => "Second article", "text" => "This is new article. Lorem ipsum dolor sit amet."),
+                array("title" => "Third article", "text" => "Hello. Lorem ipsum dolor sit amet."),
+            );
+        }
+    }
+
+    public function render(){
+        $this->moduleTemplate->articles = $this->articles;
+
+        return (string) $this->moduleTemplate;
+    }
+}
