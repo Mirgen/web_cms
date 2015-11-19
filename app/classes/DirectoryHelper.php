12739b25507e239563d71779f948e9b4ac36922d
kvapij02 <jiri.kvapil@dixonsretail.com>
2015-11-19 19:55:57 +0100

    Helper for work with directories.

---
+++ app/classes/DirectoryHelper.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Description of Directory
+ */
+class DirectoryHelper
+{
+
+    public static function recurseCopy($src,$dst) { 
+        $dir = opendir($src); 
+        @mkdir($dst); 
+        while(false !== ( $file = readdir($dir)) ) { 
+            if (( $file != '.' ) && ( $file != '..' )) { 
+                if ( is_dir($src . '/' . $file) ) { 
+                    DirectoryHelper::recurseCopy($src . '/' . $file,$dst . '/' . $file); 
+                } 
+                else { 
+                    copy($src . '/' . $file,$dst . '/' . $file); 
+                } 
+            } 
+        } 
+        closedir($dir); 
+    }
+}
