cd364df1509e4a5b1e64342a3269a5ef665dccf1
kvapilJiri <george.kvapil@seznam.cz>
2015-12-07 18:38:44 +0100

    Finally removing this "hardcoded" footer. I had to be drunk when I was doing this, because the way how I was creating the HTML of the footer menu in presenter was terrible.

--- app/FrontModule/presenters/BasePresenter.php
+++ app/FrontModule/presenters/BasePresenter.php
@@ -32,7 +32,6 @@ abstract class BasePresenter extends Nette\Application\UI\Presenter
 
             if(isset($this->template)){
                 $this->template->settings = $this->settings;
-                $this->template->menu = $this->getMenu();
             }
             $this->cache->save('pages', $this->pages);
     }
@@ -81,51 +80,5 @@ abstract class BasePresenter extends Nette\Application\UI\Presenter
             $modules[] = $module_instance->load($module->id, $this);
         }
         return $modules;
-    }
-
-    public function getMenu() {
-        $sHtml = '
-                    <ul class="nav navbar-nav">
-                        <li><a href="' . $this->link('Default:page') . '">Úvodní stránka</a></li>'
-                        . $this->getMenuRecursive() .
-                    '</ul>';
-        return $sHtml;
-    }
-
-    private function getMenuRecursive($pageId = NULL, $depth = 0,$oPages = NULL){
-        // Initialization:
-        // if we did not get submenu (which comes in $oPages), we have to load menu items (basicaly this is only at the begining)
-        if($oPages == NULL){
-            $aParameters = array('online' => '1', 'deleted' => 0, 'id_parent' => $pageId);
-            $oPages = $this->context->page->findBy($aParameters);
-        }
-
-        $sHtml = '';
-        foreach($oPages as $oPage) {
-
-            // skip page with ID = 0, because it is index/homepage of the web
-            if($oPage->id == 1){
-                continue;
-            }
-
-            $aParameters['id_parent'] = $oPage->id;
-            $oSubPages = $this->context->page->findBy($aParameters);
-            $url = $this->link('Default:page', array('id' => $oPage->id, "seo_url_text" => $oPage->final_url_text));
-
-            // prepare pages for cache, store pages data to $this->pages variable
-            $this->pages[$oPage->id] = $oPage->toArray();
-
-            if( $oSubPages->count() > 0 ){
-                $sHtml .=  '<li class="dropdown">
-                                <a href="' . $url . '" class="dropdown-toggle" data-toggle="dropdown">' . $oPage->name . ' <span class="caret"></span></a>
-                                <ul class="dropdown-menu" role="menu">
-                                    ' . $this->getMenuRecursive($oPage->id, $depth+1, $oSubPages) . '
-                                </ul>
-                            </li>';
-            } else {
-                $sHtml .= '<li><a href="' . $url . '">' . $oPage->name . '</a></li>';
-            }
-        }
-        return $sHtml;
     }
 }
