85c11e7193a967486fc8df18c7e9165382580b86
kvapilJiri <george.kvapil@seznam.cz>
2015-12-21 19:33:22 +0100

    Translations of Basic layout extended. Translation of segments was included.

--- app/FrontModule/templates/layouts/Basic/languages/cs-CZ.php
+++ app/FrontModule/templates/layouts/Basic/languages/cs-CZ.php
@@ -3,5 +3,7 @@
 $lang["title"] = "Klasické rozvržení";
 $lang["subTitle"] = "Web s hlavičkou, obsahem a patičkou";
 $lang["description"] = "Layout s hlavičkou, obsahem, patičkou. Přesně v tomto pořadí. Co vložíte do hlavičky a patičky bude na všech stránkách používajících tento layout. 2. část, obsah, bude pro každou stránku unikátní. Tyto části jsou v jednom sloupci jedna za druhou. V dnešní době hojně používaný a osvědčený model.";
+$lang["footer"] = "Patička";
+$lang["header"] = "Hlavička";
 
 return $lang;
