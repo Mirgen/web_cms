e7099c6ac42dd4567434768b9b088e4f3a77066d
kvapilJiri <george.kvapil@seznam.cz>
2015-12-22 00:12:04 +0100

    Basic layout. Incorrect ifset.

--- app/FrontModule/templates/layouts/Basic/setting.php
+++ app/FrontModule/templates/layouts/Basic/setting.php
@@ -1,6 +1,6 @@
 <?php
 
-$setting["version"] = "1.0.1";
+$setting["version"] = "1.0.2";
 $setting["creation_date"] = "2015/12/15";
 $setting["author"] = "Kvapil Jiří";
 $setting["author_email"] = "george.kvapil@seznam.cz";
